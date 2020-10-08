75417da4829a8d85f6b5d76bd5558cd4e5accb3e
v-wangbo.ea <1013166150@qq.com>
2020-10-08 17:29:00 +0800

    config

--- src/views/home/Index.js
+++ src/views/home/Index.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { Layout } from 'antd'
 import './index.scss'
 import Aside from './components/Aside'
+import HeaderMain from './components/Header'
 import MainContent from '../../componets/containerMain/index'
 const { Sider, Header, Content } = Layout
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            collapsed:true
+         }
     }
     render() { 
         return ( 
             <Layout className='layout-wrap'>
-               <Sider width='250px'>
+               <Sider width='250px' collapsed={this.state.collapsed}>
                    <Aside />
                </Sider>
                <Layout>
-                   <Header className='layout-header'>头部</Header>
+                   <Header className='layout-header'>
+                        <HeaderMain />
+                   </Header>
                    <Content className='layout-content'>
                        <MainContent/>
                    </Content>
--- src/views/home/components/Header.js
+++ src/views/home/components/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { MenuFoldOutlined } from '@ant-design/icons'
 class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
-        return (  );
+        return ( 
+            <div>
+                <span>
+                    <MenuFoldOutlined />
+                </span>
+            </div>
+         );
     }
 }
  
